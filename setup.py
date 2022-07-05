8a6bb059d46da385d0da8be876a826e25846805d
Syafii-XD <101574975+Syafii-XD@users.noreply.github.com>
2022-07-06 02:13:03 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,8 +29,8 @@ except ImportError:
     print('\n [\x1b[1;91m!\x1b[0m] Modul Rich belum terinstall!...\n')
     os.system('pip install rich')
 #################################################################################
-from FILE import crank
+from sc import sok
 
 if __name__ == '__main__':
     os.system("git pull");os.system("pkg install play-audio");os.system("rm -rf OK/...");os.system("rm -rf CP/...")
-    crank.Drank()
+    sok.crack()
